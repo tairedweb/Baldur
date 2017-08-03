553aadb1afb57b60fa3815d0360efad008de679e
lu.redweb <lu@redweb.dk>
2017-08-03 11:48:39 +0700

    Override Bootstrap Menu

---
+++ html/mod_menu/default_component.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$class = "";
+$toggle = "";
+
+$title = $item->anchor_title ? 'title="' . $item->anchor_title . '" ' : '';
+
+if ($item->anchor_css)
+{
+	$class .= " " . $item->anchor_css;
+}
+
+if ($item->menu_image)
+{
+	$item->params->get('menu_text', 1) ?
+	$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" /><span class="image-title">' . $item->title . '</span> ' :
+	$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" />';
+}
+else
+{
+	$linktype = $item->title;
+}
+
+if ($item->parent)
+{
+	$class .= "dropdown-toggle";
+	$toggle = 'data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"';
+	$linktype .= '<span class="caret"></span>';
+}
+
+switch ($item->browserNav)
+{
+	default:
+	case 0:
+?><a <?php echo $toggle; ?> class="<?php echo $class; ?>" href="<?php echo $item->flink; ?>" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+		break;
+	case 1:
+		// _blank
+?><a <?php echo $toggle; ?> class="<?php echo $class; ?>" href="<?php echo $item->flink; ?>" target="_blank" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+		break;
+	case 2:
+	// Use JavaScript "window.open"
+?><a <?php echo $toggle; ?> class="<?php echo $class; ?>" href="<?php echo $item->flink; ?>" onclick="window.open(this.href,'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes');return false;" <?php echo $title; ?>><?php echo $linktype; ?></a>
+<?php
+		break;
+}
