e32e9a0d55d03c6081cc4ac1e4cf034c88656ce2
Jerri K. G. Christiansen <jerri@redweb.dk>
2017-08-04 12:59:49 +0700

    tinypng task to compress images

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
 var replace = require('gulp-replace');
+var imagemin = require('gulp-imagemin');
+var tinypng = require('gulp-tinypng-compress');
+var cache = require('gulp-cache');
 var browserSync = require('browser-sync').create();
 
 //Run 'gulp boot-sass' prior to running 'gulp-watch'
@@ -52,6 +55,36 @@ gulp.task('scripts', function(){
 gulp.task('fonts', function() {
   return gulp.src('staging/fonts/**/*')
   .pipe(gulp.dest('dist/css/fonts'))
+});
+
+//Task to optimize images
+gulp.task('images', function(){
+  return gulp.src('staging/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({plugins: [{removeViewBox: true}]})
+  ])))
+  .pipe(gulp.dest('dist/images'))
+});
+
+//Tinypng task is the most effecient image compressor but also very time-consuming - would not use it for watch task
+gulp.task('tinypng', function () {
+    gulp.src('staging/images/**/*.{png,jpg,jpeg}')
+        .pipe(cache(tinypng({
+        //Get your own API key here: https://tinypng.com/developers 
+            key: 'OfQXJtc2DNuFfh-XCAc-7Tb7w4wmI322',
+            sigFile: 'staging/images/.tinypng-sigs',
+            log: true
+        })))
+        .pipe(gulp.dest('dist/images'));
+});
+
+//To clear the cache use this task
+gulp.task('cache:clear', function (callback) {
+    return cache.clearAll(callback)
 })
 
 //Only monitor the styles.scss file, if we set it to look after all *.scss it would include bootstrap, and that takes longer to compile. Restructure folders if we need it to monitor for multiple files or import files into styles.scss instead
@@ -64,10 +97,11 @@ gulp.task('browserSync', function() {
 });
 
 
-gulp.task('watch', ['browserSync', 'sass', 'scripts'], function (){
+gulp.task('watch', ['browserSync', 'sass', 'scripts', 'images'], function (){
     gulp.watch('staging/scss/styles.scss', ['sass']);
     gulp.watch('**/*.php', browserSync.reload); 
     gulp.watch('staging/js/*.js', ['scripts']);
+    gulp.watch('staging/images/**/*.+(png|jpg|jpeg|gif|svg)', ['images']);
 });
 
 
